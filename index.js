e54b6278aca1f5c25745cc9178c716ed536b8cfa
dlyn1110 <dlyn1110@gmail.com>
2018-03-06 00:19:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ function happyHolidays() {
   return "Happy holidays!";
 }
 function happyHolidaysTo(name) {
+  var name  = "you";
   return 'Happy holidays, ${name}!';
 }
 function happyHolidayTo(holiday, name) {
   return 'Happy ${holiday}, ${name}!';
+  var name = "Independence Day";
+  var name = "you!"
 }
 function holidayCountdown(holiday, days) {
   return 'It\'s ${days} days until ${holiday}!';
